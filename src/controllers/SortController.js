5918fe7649d0dcaae4020fb5eda592031509364c
Gabriel Hahn Schaeffer <gabriel_hahn@hotmail.com>
2020-05-27 16:09:34 -0300

    Sort types click events added to VisualController

---
+++ src/controllers/SortController.js
@@ -0,0 +1,9 @@
+import bubbleSort from '../algorithms/bubbleSort';
+
+class SortController {
+  constructor() {
+    
+  }
+}
+
+export default new SortController();
--- webpack.config.js
+++ webpack.config.js
@@ -5,6 +5,6 @@ module.exports = {
   output: {
     filename: 'bundle.js',
     path: path.join(__dirname, 'dist'),
-    publicPath: 'dist'
-  }
+    publicPath: 'dist',
+  },
 };
